a023822157a22dc6f9de2a4276ba5c7cc4ad93a2
Porter Hoskins <porterhoskins@gmail.com>
2017-07-21 23:36:16 -0600

    Created App

---
+++ More2Life/More2Life/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  More2Life
+//
+//  Created by Porter Hoskins on 7/21/17.
+//  Copyright © 2017 Domo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
